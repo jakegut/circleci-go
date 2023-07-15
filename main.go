47e79a27327386f2247d9ca536ed6da9b76d7a8e
Jake Gutierrez <jakegut0108@gmail.com>
2023-07-15 13:09:20 -0500

    slight go file change

--- main.go
+++ main.go
@@ -18,5 +18,10 @@ func main() {
 			"message": "ayylamo",
 		})
 	})
+	r.GET("/gome", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "gome",
+		})
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }

783bf265774b5bceaa23974314a90b51b578a103
Jake Gutierrez <jakegut0108@gmail.com>
2023-07-15 13:22:31 -0500

    try using docker container driver

--- main.go
+++ main.go
@@ -18,9 +18,9 @@ func main() {
 			"message": "ayylamo",
 		})
 	})
-	r.GET("/gome", func(c *gin.Context) {
+	r.GET("/gnome", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "gome",
+			"message": "gnome",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
